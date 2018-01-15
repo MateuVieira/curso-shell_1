9febaf0d058595306c23ad6c17b561dc3191cc54
Mateus V <mateus12v@hotmail.com>
2018-01-15 11:13:54 -0200

    add if -> dir png

--- scipts/corversao-jpg-png_2.sh
+++ scipts/corversao-jpg-png_2.sh
@@ -2,8 +2,13 @@
 
 cd ~/workspace/Projetos/curso-shell_1/imagens-livros
 
+if { ! -d ong }
+then
+	mkdir png
+fi
+
 for imagen in *.jpg 
 do
 	imagem_sem_extensao=$(ls $imagen | awk -F. '{ print $1 }')
-	convert $imagem_sem_extensao.jpg $imagem_sem_extensao.png
+	convert $imagem_sem_extensao.jpg png/$imagem_sem_extensao.png
 done
