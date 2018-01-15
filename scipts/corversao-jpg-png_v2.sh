0bc39356a75315bd5246cdb6901453274abeb5ec
Mateus V <mateus12v@hotmail.com>
2018-01-15 11:57:34 -0200

    estrutura basica

---
+++ scipts/corversao-jpg-png_v2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd ~/workspace/Projetos/curso-shell_1/imagens-novos-livros
+
+for arquivo in *
+do
+
+	if [ -d $arquivo ]
+	then
+		#Entrar no diretorio e fazer varredura do conteudo
+	else 
+		#Conversao jpg para pgn
+	fi
+
+done
