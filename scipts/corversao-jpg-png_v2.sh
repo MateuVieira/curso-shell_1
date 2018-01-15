408a561ddca45be851d02e35bced9f6c23fa3bad
Mateus V <mateus12v@hotmail.com>
2018-01-15 12:19:19 -0200

    teste script

--- scipts/corversao-jpg-png_v2.sh
+++ scipts/corversao-jpg-png_v2.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 
-~/workspace/Projetos/curso-shell_1/imagens-novos-livros
 
+
+converte_imagem(){
+	
+	local caminho_imagem=$1
+	local imagem_sem_extemsao =$(ls $caminho_imagem | awk -F. '{ print $1 }')
+	convert $imagem_sem_extensao.jpg $imagem_sem_exensao.png
+}
 varredura(){
 	cd $1
 	for arquivo in *
 	do
-
-		if [ -d $arquivo ]
+		local caminho_arquivo=$(find ~/workspace/Projetos/curso-shell_1/imagens-novos-livros -name $arquivo)
+		if [ -d $caminho_arquivo ]
 		then
-			varredura $arquivo
+			varredura $caminho_arquivo
 		else 
-			#Conversao jpg para pgn
+			converte_imagem $caminho_arquivo
 		fi
 
 	done
 }
+
+varrer_diretorio ~/workspace/Projetos/curso-shell_1/imagens-novos-livros 2>
+
+if [ $? -eq 0 ]
+then 
+	echo "Conversao realizada com sucesso"
+else 
+	echo "Houve um erro no processo"
+fi
+
