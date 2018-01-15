23ca1aed212c04096a443c4655f4ec95d835e8fd
Mateus V <mateus12v@hotmail.com>
2018-01-15 12:08:51 -0200

    add function

--- scipts/corversao-jpg-png_v2.sh
+++ scipts/corversao-jpg-png_v2.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-cd ~/workspace/Projetos/curso-shell_1/imagens-novos-livros
+~/workspace/Projetos/curso-shell_1/imagens-novos-livros
 
-for arquivo in *
-do
+varredura(){
+	cd $1
+	for arquivo in *
+	do
 
-	if [ -d $arquivo ]
-	then
-		#Entrar no diretorio e fazer varredura do conteudo
-	else 
-		#Conversao jpg para pgn
-	fi
+		if [ -d $arquivo ]
+		then
+			varredura $arquivo
+		else 
+			#Conversao jpg para pgn
+		fi
 
-done
+	done
+}
