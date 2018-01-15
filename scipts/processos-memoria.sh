bae35c2f26168ae1dacf586aee982a942887e4ae
Mateus V <mateus12v@hotmail.com>
2018-01-15 13:48:33 -0200

    correcao bug -> ps tamanho

--- scipts/processos-memoria.sh
+++ scipts/processos-memoria.sh
@@ -6,15 +6,16 @@ then
 fi
 
 processos_memoria(){
-processos=$(ps -e -o pid --sort -size | head -n 11| grep [0-9])
 
-for pid in $processos
-do
-	local nome_processo=$(ps -p $pid -o comm=)
-	echo -n $(date +%F,%H:%M:%S,) >> $nome_processo.log
-	local tamanho_processo=$(ps -p $pid size | grep [0-9])
-	echo "$(bc <<< "scale=2;$tamanho_processo/1024") MB" >> $nome_processo.log
-done
+	processos=$(ps -e -o pid --sort -size | head -n 11 | grep [0-9])
+
+	for pid in $processos
+	do
+		local nome_processo=$(ps -p $pid -o comm=)
+		echo -n $(date +%F,%H:%M:%S,) >> $nome_processo.log
+		local tamanho_processo=$(ps -p $pid -o size | grep [0-9])
+		echo "$(bc <<< "scale=2;$tamanho_processo/1024") MB" >> $nome_processo.log
+	done
 }
 
 processos_memoria
