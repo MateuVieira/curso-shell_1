01dbcd4e8010c0700f73043dedc3a78abdf7af5b
Mateus V <mateus12v@hotmail.com>
2018-01-15 11:08:56 -0200

    correcao de erro

--- scipts/corversao-jpg-png_2.sh
+++ scipts/corversao-jpg-png_2.sh
@@ -4,6 +4,6 @@ cd ~/workspace/Projetos/curso-shell_1/imagens-livros
 
 for imagen in *.jpg 
 do
-	imagem_sem_extensao = $(ls $imagen| awk -F. '{ print $1 }')
-	convert $imagen_sem_extensao.jpg $imagen_sem_extensao.png
+	imagem_sem_extensao=$(ls $imagen | awk -F. '{ print $1 }')
+	convert $imagem_sem_extensao.jpg $imagem_sem_extensao.png
 done
