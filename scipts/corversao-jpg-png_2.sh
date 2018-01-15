55b70110c7afa5c8acbb0fa5eedf83c7c26d0365
Mateus V <mateus12v@hotmail.com>
2018-01-15 10:51:40 -0200

    all images

--- scipts/corversao-jpg-png_2.sh
+++ scipts/corversao-jpg-png_2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-caminho_imagens = ~/workspace/Projetos/curso-shell_1/imagens-livros
+cd ~/workspace/Projetos/curso-shell_1/imagens-livros
 
-for imagen in $@
+for imagen in *.jpg
 do
-	convert $caminho_imagens/$imagen.jpg $caminho_imagens/$imagen.png
+	convert $imagen.jpg $imagen.png
 done
