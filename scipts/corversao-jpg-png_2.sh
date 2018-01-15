b75639ce8d9b77957fdee7b6254f460d0dc99690
Mateus V <mateus12v@hotmail.com>
2018-01-15 11:38:53 -0200

    corecao erro function

--- scipts/corversao-jpg-png_2.sh
+++ scipts/corversao-jpg-png_2.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
+converte_imagem(){
 cd ~/workspace/Projetos/curso-shell_1/imagens-livros
 
-if [ ! -d ong ]
+if [ ! -d png ]
 then
 	mkdir png
 fi
 
+local imagen
+
 for imagen in *.jpg 
 do
-	imagem_sem_extensao=$(ls $imagen | awk -F. '{ print $1 }')
+	local imagem_sem_extensao=$(ls $imagen | awk -F. '{ print $1 }')
 	convert $imagem_sem_extensao.jpg png/$imagem_sem_extensao.png
 done
+}
+
+converte_imagem
+
+if [ $? -eq 0 ]
+then 
+	echo "Conversão realizada com sucesso"
+else
+	echo "Houve uma falar no processo"
+fi
