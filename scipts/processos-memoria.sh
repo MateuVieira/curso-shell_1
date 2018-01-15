3d35a88e51bc3eca92f829b17ee4c248469a6874
Mateus V <mateus12v@hotmail.com>
2018-01-15 13:43:31 -0200

    teste implementação

--- scipts/processos-memoria.sh
+++ scipts/processos-memoria.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
+if [ ! -d log]
+then
+	mkdir log
+fi
+
+processos_memoria(){
 processos=$(ps -e -o pid --sort -size | head -n 11| grep [0-9])
 
 for pid in $processos
 do
-	echo $(ps -p $pid -o comm=)
+	local nome_processo=$(ps -p $pid -o comm=)
+	echo -n $(date +%F,%H:%M:%S,) >> $nome_processo.log
+	local tamanho_processo=$(ps -p $pid size | grep [0-9])
+	echo "$(bc <<< "scale=2;$tamanho_processo/1024") MB" >> $nome_processo.log
 done
+}
+
+processos_memoria
+
+if [ $? -eq 0]
+then
+	echo "Os arquivos foram salvos com sucesso"
+else
+	echo "Houve um problema durante o processo"
+fi
